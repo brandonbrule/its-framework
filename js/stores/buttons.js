f5a4840b8b421c32dde717df569ad223c7a07d72
brandonbrule <brandonbrule@gmail.com>
2016-02-13 14:44:19 -0500

    json export organized

--- js/stores/buttons.js
+++ js/stores/buttons.js
@@ -28,33 +28,3 @@ var Buttons = (function() {
   };
 
 })();
-
-
-// Functions
-var Export = (function() {
-  var exportdata = {};
-
-  var update = function(){
-    var str = json(exportdata);
-    var el = document.querySelector('[its-view="data-export"]');
-    el.innerHTML=str;
-  }
-
-  var json = function(obj){
-    return JSON.stringify(obj);
-  };
-  
-  var init = function(data) {
-    
-    if(data.control){
-      exportdata[data.control] = data.element.value;
-      update();
-    }
-    
-  };
-
-  return {
-    init: init
-  };
-
-})();
