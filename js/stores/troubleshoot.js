4481d64c1dd227d1d68ab4d59c70c62c712abc78
brandonbrule <brandonbrule@gmail.com>
2016-02-14 19:50:37 -0500

    troubleshooting moved

--- js/stores/troubleshoot.js
+++ js/stores/troubleshoot.js
@@ -2,10 +2,22 @@
 var Troubleshoot = (function() {
   
   var control = function(data) {
-    if(data.control){
-      its.clearAll();
-      its.a(data);
+
+  	if (data.control === 'wut'){
+    	if (its_container_wrapper.parentNode.style.display === 'block'){
+    		its_container_wrapper.parentNode.style.display = 'none';
+    	} else {
+    		its_container_wrapper.parentNode.style.display = 'block';
+    	}
     }
+
+    if(data.control){
+    	if (its_container_wrapper.parentNode.style.display === 'block'){
+      		its.clearAll();
+      		its.a(data);
+  		}
+    }
+    
   };
 
   return {
