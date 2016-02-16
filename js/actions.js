809f018529d1682eaab63bf9a7c5ff7a9ec6b2e1
brandonbrule <brandonbrule@gmail.com>
2016-02-16 17:18:13 -0500

    remove keydown

--- js/actions.js
+++ js/actions.js
@@ -13,14 +13,6 @@ document.addEventListener("keydown", function(e) {
   		Dispatch.init(e);
 	}
 });
-document.addEventListener("keyup", function(e) {
-	// Tab shift Tab doesn't trigger change (accessibility)
-	// Ha, interesting, Spacebar on button registers as click
-	// So it sends click and keypress with just keypress - only on buttons.
-	if (event.keyCode !== 9 && event.keyCode !== 16 && event.keyCode !== 32 && event.keyCode !== 13) {
-  		Dispatch.init(e);
-	}
-});
 
 // Change Event
 document.addEventListener("change", function(e) {
