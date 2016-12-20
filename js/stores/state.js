6d679b85f1aa9ba80102f3052fa444c425fce25a
brandonbrule <brandonbrule@gmail.com>
2016-12-19 20:00:57 -0500

    state onload

--- js/stores/state.js
+++ js/stores/state.js
@@ -9,43 +9,40 @@ var State = (function() {
   }
 
   var init = function(data) {
-    
-
-    if(data.control){
       
-      // Don't keep track of submit values
-      if(data.type !== 'submit'){
-        state[data.control] = data.element.value;
-      }
+    // Don't keep track of submit values
+    if(data.type !== 'submit'){
+      state[data.control] = data.element.value;
+    }
 
-      // If empty delete object property
-      if (state[data.control] === ''){
+    // If empty delete object property
+    if (state[data.control] === '' || state[data.control] !== null){
+      delete state[data.control];
+    }
+
+    // If it's a button and the state isn't changed
+    // This sets up button values as toggle-able
+    if (data.element_type === 'BUTTON'){
+      if (data.changed === false){
         delete state[data.control];
       }
+    }
 
-      // If it's a button and the state isn't changed
-      // This sets up button values as toggle-able
-      if (data.element_type === 'BUTTON'){
-        if (data.changed === false){
-          delete state[data.control];
-        }
+    // If checkbox and its unchecked delete property
+    if (data.type === 'checkbox'){
+      if (!data.element.checked){
+        delete state[data.control]
       }
+    }
 
-      // If checkbox and its unchecked delete property
-      if (data.type === 'checkbox'){
-        if (!data.element.checked){
-          delete state[data.control]
-        }
-      }
-
-      
-      if(data.event_type === 'load'){
-        var controls = document.querySelectorAll('[its-control]');
-        [].forEach.call(controls, function(control){
+    
+    if(data.event_type === 'load'){
+      var controls = document.querySelectorAll('[its-control]');
+      [].forEach.call(controls, function(control){
+        if(control.value){
           state[control.getAttribute('its-control')] = control.value;
-        });
-      }
-
+        }
+      });
     }
 
     return state;
