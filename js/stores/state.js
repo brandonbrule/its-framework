618abbc0db3706ccb00c650a7573b5b186502645
brandonbrule <brandonbrule@gmail.com>
2016-12-19 19:53:57 -0500

    if state is not null

--- js/stores/state.js
+++ js/stores/state.js
@@ -10,37 +10,42 @@ var State = (function() {
 
   var init = function(data) {
     
-    // Don't keep track of submit values
-    if(data.type !== 'submit'){
-      state[data.control] = data.element.value;
-    }
 
-    // If empty delete object property
-    if (state[data.control] === ''){
-      delete state[data.control];
-    }
+    if(data.control){
+      
+      // Don't keep track of submit values
+      if(data.type !== 'submit'){
+        state[data.control] = data.element.value;
+      }
 
-    // If it's a button and the state isn't changed
-    // This sets up button values as toggle-able
-    if (data.element_type === 'BUTTON'){
-      if (data.changed === false){
+      // If empty delete object property
+      if (state[data.control] === ''){
         delete state[data.control];
       }
-    }
 
-    // If checkbox and its unchecked delete property
-    if (data.type === 'checkbox'){
-      if (!data.element.checked){
-        delete state[data.control]
+      // If it's a button and the state isn't changed
+      // This sets up button values as toggle-able
+      if (data.element_type === 'BUTTON'){
+        if (data.changed === false){
+          delete state[data.control];
+        }
       }
-    }
 
-    
-    if(data.event_type === 'load'){
-      var controls = document.querySelectorAll('[its-control]');
-      [].forEach.call(controls, function(control){
-        state[control.getAttribute('its-control')] = control.value;
-      });
+      // If checkbox and its unchecked delete property
+      if (data.type === 'checkbox'){
+        if (!data.element.checked){
+          delete state[data.control]
+        }
+      }
+
+      
+      if(data.event_type === 'load'){
+        var controls = document.querySelectorAll('[its-control]');
+        [].forEach.call(controls, function(control){
+          state[control.getAttribute('its-control')] = control.value;
+        });
+      }
+
     }
 
     return state;
