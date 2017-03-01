82ee95d1373d1fe64c48c2a9cbef62334a41d885
brandonbrule <brandonbrule@gmail.com>
2017-02-28 22:27:56 -0500

    oops no itsa

--- js/actions.js
+++ js/actions.js
@@ -9,7 +9,6 @@ document.addEventListener("keyup", function(e) {
 	// Tab shift Tab doesn't trigger change (accessibility)
 	// Ha, interesting, Spacebar on button registers as click
 	// So it sends click and keypress with just keypress - only on buttons.
-	its.a(e.keyCode);
 	if (e.keyCode !== 9 && e.keyCode !== 16 && e.keyCode !== 32) {
   		Dispatch.init(e);
 	}
