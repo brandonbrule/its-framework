49f2216c3210e91c9276645bf0d5855749fe081b
brandonbrule <brandonbrule@gmail.com>
2017-04-23 18:15:20 -0400

    Nested Object Properties

--- js/state.js
+++ js/state.js
@@ -9,6 +9,62 @@ var State = (function() {
     return state;
   }
 
+  var propObj = function(state, str_arr, value){
+    lastKeyIndex = str_arr.length-1;
+   for (var i = 0; i < lastKeyIndex; ++ i) {
+     key = str_arr[i];
+
+     if (!(key in state)){
+       state[key] = {}
+     }
+     state = state[key];
+     
+   }
+   state[str_arr[lastKeyIndex]] = value;
+  }
+
+  var buildObjectFromString = function(str, value){
+    var str_arr = str.split('.');
+    propObj(state, str_arr, value);
+  };
+
+  // This function will return a property inside a nested object by string.
+  // This function needs two arguments
+  // The property "prop", "prop.prop", "prop.obj.prop" - as a string.
+  // The master object the properties are referencing.
+  var returnNestedProperty = function(str, master_object) {
+    
+    // Break String Up by Period
+    // This will form a new array of each "property"
+    var str_array = str.split('.');
+    
+    // Hold a copy of each nested object properties.
+    var tmp_obj = null;
+
+    // Iterate over the input properties
+    for (var i = 0; i < str_array.length; i++) {
+      var current = str_array[i];
+      
+      // If tmp_obj initially is null
+      // On the first iteration of object properties
+      // We assign the first property of the State object to tmp_object
+      if (!tmp_obj) {
+        tmp_obj = master_object[current];
+        
+      // If tmp_obj is an object already
+      // We will be on the next level of the object
+      // At this level we're going to set tmp_obj
+      // To the next nested object property.
+      } else {
+        tmp_obj = tmp_obj[current];
+      }
+      
+    }
+
+    return tmp_obj;
+
+  };
+
 
   // A Hard Refresh based off Element Info
   // Queries all its-control elements, and updates
@@ -19,75 +75,101 @@ var State = (function() {
     var controls = document.querySelectorAll('[its-control]');
       [].forEach.call(controls, function(control){
           var control_type = control.getAttribute('its-control');
+          var control_value = control.value;
+          
+          // its-control="OBJ.obj.prop"
+          if(control_type.indexOf('.') !== -1){
+            buildObjectFromString(control_type, control_value);
 
-          if (control.nodeName === 'BUTTON'){
-            if (control.getAttribute('its-active') === 'true'){
-              state[control_type] = control.value;
-            }
+
+          // Standard state.prop relationship
           } else {
 
-            switch(control.getAttribute('type')) {
-              case 'checkbox':
-                  if (control.checked){
-                    state[control_type] = control.value;
-                  }
-                  break;
-              case 'radio':
-                  if (control.checked){
-                    state[control_type] = control.value;
-                  }
-                  break;
-              default:
-
-                if (control.getAttribute('type') !== 'submit'){
-                  if(control.value.length > 0){
-                    state[control_type] = control.value;
-                  }
-
-                  if(state[control_type]){
-                    control.value = state[control_type];
-                  }
-                }
-                break;
+            if (control.nodeName === 'BUTTON'){
+              if (control.getAttribute('its-active') === 'true'){
+                state[control_type] = control_value;
               }
+            } else {
 
+              switch(control.getAttribute('type')) {
+                case 'checkbox':
+                    if (control.checked){
+                      state[control_type] = control_value;
+                    }
+                    break;
+                case 'radio':
+                    if (control.checked){
+                      state[control_type] = control_value;
+                    }
+                    break;
+                default:
+
+                  if (control.getAttribute('type') !== 'submit'){
+                    if(control_value.length > 0){
+                      state[control_type] = control_value;
+                    }
+
+                    if(state[control_type]){
+                      control_value = state[control_type];
+                    }
+                  }
+                  break;
+                }
+
+            }
           }
-
       });
   };
 
   var init = function(data) {
+
+
+    if(data.control){
       
-    // Don't keep track of submit values
-    if(data.type !== 'submit'){
-      state[data.control] = data.element.value;
-    }
 
-    // If it's a button and the state isn't changed
-    // This sets up button values as toggle-able
-    if (data.nodeName === 'BUTTON'){
-      if (!data.element.value){
-        delete state[data.control];
+      // If there's . in prop name treat it like nested object
+      if(data.control.indexOf('.') !== -1){
+        buildObjectFromString(data.control, data.value);
+      
+
+      // Just a regular property item value
+      }  else {
+
+        // Don't keep track of submit values
+        if(data.type !== 'submit'){
+          state[data.control] = data.element.value;
+        }
+
+        // If it's a button and the state isn't changed
+        // This sets up button values as toggle-able
+        if (data.nodeName === 'BUTTON'){
+          if (!data.element.value){
+            delete state[data.control];
+          }
+        }
+
+        // If checkbox and its unchecked delete property
+        if (data.type === 'checkbox'){
+          if (!data.element.checked){
+            delete state[data.control];
+          }
+        }
+
+        // If there's nothing, disregard from state.
+        // (state obj.prop will still be undefined)
+        if (state['null']){
+          delete state['null'];
+        }
+
+        if(state['']){
+          delete state[''];
+        }
+
       }
-    }
-
-    // If checkbox and its unchecked delete property
-    if (data.type === 'checkbox'){
-      if (!data.element.checked){
-        delete state[data.control];
-      }
-    }
-
-    // If there's nothing, disregard from state.
-    // (state obj.prop will still be undefined)
-    if (state['null']){
-      delete state['null'];
+      
     }
     
-    if(state['']){
-      its.a('test');
-      delete state[''];
-    }
+    
 
     
     // Initial Load
@@ -99,6 +181,7 @@ var State = (function() {
     if(data.event_type === 'load'){
       updateAll();
     }
+
 
     // Yay Here's the State Object
     // Initial load is a bit, but smarter afterwards matching
